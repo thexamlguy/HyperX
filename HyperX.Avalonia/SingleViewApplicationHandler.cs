9c3535fa67df91c341ae7260ccb8ef92806b84a2
TheXamlGuy <dan_clark@outlook.com>
2024-02-28 20:55:09 +0000

    Add SingleViewApplicationHandler

--- HyperX.Avalonia/SingleViewApplicationHandler.cs
+++ HyperX.Avalonia/SingleViewApplicationHandler.cs
@@ -1,13 +1,27 @@
-﻿using Avalonia.Controls.ApplicationLifetimes;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
 
 namespace HyperX.Avalonia;
 
-public class SingleViewApplicationHandler :
+public class SingleViewApplicationHandler(IViewModelContentBinder binder) :
     INavigateHandler<ISingleViewApplicationLifetime>
 {
     public Task Handle(Navigate<ISingleViewApplicationLifetime> args,
         CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        if (Application.Current?.ApplicationLifetime is
+            ISingleViewApplicationLifetime lifeTime)
+        {
+            if (args.View is ContentControl content)
+            {
+                lifeTime.MainView = content;
+                content.DataContext = args.ViewModel;
+
+                binder.Bind(content);
+            }
+        }
+
+        return Task.CompletedTask;
     }
 }
